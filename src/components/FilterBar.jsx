156c7a345313302fee9b3d2504b6583d91450af5
MiyaAadil <miyaaadil76@gmail.com>
2026-06-30 12:51:51 +0545

    Searching, filtering and sorting features done

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+const categories = [
+    "all",
+    "electronics",
+    "jewelery",
+    "men's clothing",
+    "women's clothing"
+];
 
-const FilterBar = ({ searchTerm, setSearchTerm, selectedCategory, setSelectedCategory}) => {
+const FilterBar = ({ searchTerm, setSearchTerm, selectedCategory, setSelectedCategory, sortOption, setSortOption}) => {
+
   return (
     <div className="flex flex-col gap-4 mt-4 mx-5">
 
@@ -16,41 +23,35 @@ const FilterBar = ({ searchTerm, setSearchTerm, selectedCategory, setSelectedCat
         {/* Category */}
         <div className="flex flex-wrap gap-2 cursor-pointer">
 
-            <button className="px-4 py-2 bg-blue-900 text-white rounded cursor-pointer">
-            All
-            </button>
-
-            <button className="px-4 py-2 bg-gray-200 rounded cursor-pointer">
-            Electronics
-            </button>
-
-            <button className="px-4 py-2 bg-gray-200 rounded cursor-pointer">
-            Jewelery
-            </button>
-
-            <button className="px-4 py-2 bg-gray-200 rounded cursor-pointer">
-            Men's Clothing
-            </button>
-
-            <button className="px-4 py-2 bg-gray-200 rounded cursor-pointer">
-            Women's Clothing
-            </button>
-
+            {categories.map((category) => (
+                <button
+                key={category}
+                onClick={() => setSelectedCategory(category)}
+                className={`px-4 py-2 rounded capitalize transition-all cursor-pointer ${
+                    selectedCategory === category
+                    ? "bg-blue-900 text-white"
+                    : "bg-gray-200 text-black hover:bg-gray-300"
+                }`}
+                >
+                {category}
+                </button>
+            ))}
         </div>
 
         {/* Sort */}
 
-        <select className="border p-3 rounded-lg w-60 cursor-pointer">
+        <select className="border p-3 rounded-lg w-60 cursor-pointer"
+        value={sortOption} onChange={(e) => setSortOption(e.target.value)}>
 
-            <option>Default</option>
+            <option value="default">Default</option>
 
-            <option>Price: Low to High</option>
+            <option value="low-high">Price: Low to High</option>
 
-            <option>Price: High to Low</option>
+            <option value="high-low">Price: High to Low</option>
 
-            <option>Name: A-Z</option>
+            <option value="a-z">Name: A-Z</option>
 
-            <option>Name: Z-A</option>
+            <option value="z-a">Name: Z-A</option>
 
         </select>
 
@@ -58,4 +59,4 @@ const FilterBar = ({ searchTerm, setSearchTerm, selectedCategory, setSelectedCat
   )
 }
 
-export default FilterBar
+export default FilterBar;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import { getProducts } from "../services/api";
 import ProductGrid from "../components/ProductGrid";
 import FilterBar from "../components/FilterBar";
+import { Filter } from "lucide-react";
 
 const Products = () => {
 
   const [products, setProducts] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("all");
+  const [sortOption, setSortOption] = useState("default");
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -34,17 +36,35 @@ const Products = () => {
 
     return matchesSearch && matchesCategory;
   });
-
-
   
+
+  const sortedProducts = [...filteredProducts]
+  if (sortOption === "low-high") {
+    sortedProducts.sort((a,b) => a.price - b.price)
+  }
+
+  if (sortOption === "high-low") {
+  sortedProducts.sort((a, b) => b.price - a.price);
+  }
+
+  if (sortOption === "a-z") {
+    sortedProducts.sort((a, b) => a.title.localeCompare(b.title));
+  }
+
+  if (sortOption === "z-a") {
+    sortedProducts.sort((a, b) => b.title.localeCompare(a.title));
+  }
+
+
+
   return (
     <div className="flex flex-col justify-center items-center">
 
       <h1 className="text-3xl font-bold text-blue-950 mt-6">All Products</h1>
 
-      <FilterBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+      <FilterBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} sortOption={sortOption} setSortOption={setSortOption} />
 
-      <ProductGrid products={filteredProducts} />
+      <ProductGrid products={sortedProducts} />
     </div>
   );
 };
